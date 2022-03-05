8ae6a994bbb81693b47b9c511aaafd456a779384
ashloh1991 <lohiaashish08@gmail.com>
2022-03-05 22:42:49 +0530

    added comments

--- MyFirstApp/MyFirstApp/Number.cs
+++ MyFirstApp/MyFirstApp/Number.cs
@@ -6,7 +6,9 @@ namespace MyFirstApp
 {
     public static class Number
     {
-
+        /// <summary>
+        /// print first 100 numbers
+        /// </summary>
         public static void PrintFirstHundredNumbers()
         {
             for (int i = 0; i < 100; i++)
