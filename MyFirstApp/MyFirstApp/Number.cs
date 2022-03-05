d40a0b4d7106ab3d13c07ea034d75c9d8d0bd37a
ashloh1991 <lohiaashish08@gmail.com>
2022-03-05 22:35:54 +0530

    first commit

---
+++ MyFirstApp/MyFirstApp/Number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstApp
+{
+    public static class Number
+    {
+
+        public static void PrintFirstHundredNumbers()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
